8816d53119b3b2efd06b20612bcb5ec3e31c741c
on-jungwoan <slalfpdl16@naver.com>
2023-09-07 07:33:33 +0900

    [fix] MSDA 연산 시 fp32로 강제 캐스팅

--- models/dn_dab_dino_deformable_detr/ops/functions/ms_deform_attn_func.py
+++ models/dn_dab_dino_deformable_detr/ops/functions/ms_deform_attn_func.py
@@ -17,13 +17,12 @@ from torch.autograd.function import once_differentiable
 
 import MultiScaleDeformableAttention as MSDA
 
-
 class MSDeformAttnFunction(Function):
     @staticmethod
     def forward(ctx, value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, im2col_step):
         ctx.im2col_step = im2col_step
         output = MSDA.ms_deform_attn_forward(
-            value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, ctx.im2col_step)
+            value.type(torch.float32), value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights, ctx.im2col_step)
         ctx.save_for_backward(value, value_spatial_shapes, value_level_start_index, sampling_locations, attention_weights)
         return output
 
